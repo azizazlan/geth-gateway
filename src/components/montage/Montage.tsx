0405cc2e9db070aeb78c044815a178be1d23ca8f
farabi <mac@macs-MacBook-Pro.local>
2022-12-14 17:48:14 +0800

    Add image in montage as placeholder

--- src/components/montage/Montage.tsx
+++ src/components/montage/Montage.tsx
@@ -2,7 +2,14 @@ import * as React from 'react';
 import styles from './styles';
 
 function Montage() {
-  return <div style={styles.montage}>todo add image here</div>;
+  return (
+    <div style={styles.montage}>
+      <img
+        src="https://reactnativecode.com/wp-content/uploads/2018/02/Default_Image_Thumbnail.png"
+        alt="placeholder"
+      />
+    </div>
+  );
 }
 
 export default Montage;
