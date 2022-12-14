203c6340a6467ae84e62291970422516d25be7d4
Azlan <azlan@thuleen.io>
2022-12-14 15:24:16 +0800

    Make the montage to be at center

---
+++ src/components/montage/styles.ts
@@ -0,0 +1,11 @@
+import theme from '../../theme';
+
+const styles: { [key: string]: React.CSSProperties } = {
+  montage: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+};
+export default styles;
